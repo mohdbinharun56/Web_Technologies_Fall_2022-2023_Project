8ea657653b2b7dadd4cfe08f230b8c50c999fdb0
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-11-26 20:42:49 +0600

    Update Database.php

--- models/Database.php
+++ models/Database.php
@@ -145,4 +145,176 @@ function getaddAppointment($id,$patient_name,$doctor_name,$department,$Date,$tim
       header("Location:../views/AppointmentView.php");
     }
 }
+
+//delete appointment
+function getdeleteAppointment($delid){
+    $con = getconection();
+    $sql="delete from appointment WHERE SLNo ='$delid';";
+    mysqli_query($con,$sql);
+}
+
+//update appointment
+function getupdateAppointment($appointmentid,$patientname,$doctorname,$department,$appointmentdate,$serial,$slno){
+    $con = getconection();
+    $sql="UPDATE `appointment` SET `AppoinmentID`='$appointmentid',`Patient_name`='$patientname',`Doctor_name`='$doctorname',`Department`='  $department',`Appoinment_date`='$appointmentdate',`Serial`=$serial WHERE SLNo=$slno;";
+    mysqli_query($con,$sql);
+}
+
+//select appointment
+function getselectappointment($id){
+    $con = getconection();
+    $sql="select * from appointment where SLNo='$id'";
+    $result=mysqli_query($con,$sql);
+    return $result;
+}
+
+//insert transaction 
+function getinserttransaction($appointmentid,$patient_name,$doctor_name,$serial,$payment,$date,$time,$payment_type,$status){
+    $con = getconection();
+    //check the appointment id is unique or already exist.
+    $sql ="SELECT * from invoice where Appointment_ID='$appointmentid'";
+    $result = mysqli_query($con,$sql);
+    
+    if($result->num_rows > 0){
+      header("location:../views/AppointmentView.php?print=This appointment id already exist!");
+    }
+    else{
+      $sql = "INSERT INTO `invoice`(`Appointment_ID`,`Patient_Name`, `Doctor_Name`,`Serial`, `Payment`, `Appointment_Date`,`Appoinment_time`, `Payment_Type`, `Status`) VALUES ('$appointmentid','$patient_name','$doctor_name',$serial,'$payment','$date','$time','$payment_type','$status');";
+      mysqli_query($con,$sql);
+      header("Location:../views/Invoice.php");
+    }
+    
+}
+
+//view invoice 
+function getviewInvoice(){
+    $con = getconection();
+    $sql = "select * from invoice order by Appointment_ID";
+    $result = mysqli_query($con,$sql);
+    return $result;
+}
+
+//delete invoice
+function getdeleteinvoice($sl){
+    $con = getconection();
+    $sql="delete from invoice WHERE SLNo='$sl'";
+    mysqli_query($con,$sql);
+}
+
+//search status on invoice
+function getsearchstatus($status){
+    $con = getconection();
+    $sql1 = "select * from invoice where Status = '$status'";
+    $result=mysqli_query($con,$sql1);
+    return $result;
+}
+
+//view Doctor Appointment Date, DAy, Time schedule
+function gettimeschedule(){
+    $con = getconection();
+    $sql = "select * from allowed_date_time order by Doctor_name";
+    $result = mysqli_query($con,$sql);
+    return $result;
+} 
+
+//insert Doctor Appointment Date, DAy, Time schedule
+function getinsertTimeschedule($selectdate,$selecttime,$selectdocotor,$department){
+    $con = getconection();
+    $sql1 = "insert into allowed_date_time (Date,Time,Doctor_name,Department) values ('$selectdate','$selecttime','$selectdocotor','$department')";
+    mysqli_query($con,$sql1);
+}
+
+//Transition Dashboard
+//total payment 
+function gettotalpayment(){
+    $con = getconection();
+    $sql = "select SUM(Payment) as total_sum from invoice where Status = 'Paid'";
+    $resultsum = mysqli_query($con,$sql);
+    $r=mysqli_fetch_assoc($resultsum);
+    $totalpayment = $r['total_sum'];
+    return $totalpayment;
+}
+
+//total due payment
+function gettotalduepayment(){
+    $con = getconection();
+    $sql1 = "select SUM(Payment) as total_due from invoice where Status = 'Unpaid'";
+    $resultdue = mysqli_query($con,$sql1);
+    $r=mysqli_fetch_assoc($resultdue);
+    if($r > 0){
+        $totaldue = $r['total_due'];
+    }else{
+        $totaldue=0;
+    }
+    return $totaldue;
+}
+
+//count numer of paid payment 
+function getcountpaid(){
+    $con = getconection();
+    $sql2 = "select count(*) as count_paid from invoice where Status = 'Paid'";
+    $resultpaid = mysqli_query($con,$sql2);
+    $r=mysqli_fetch_assoc($resultpaid);
+    if($r > 0){
+        $totalpaid = $r['count_paid'];
+    }else{
+        $totalpaid=0;
+    }
+    return $totalpaid;
+}
+
+//count number of unpaid payment
+function getcountunpaid(){
+    $con = getconection();
+    $sql3 = "select count(*) as count_unpaid from invoice where Status = 'Unpaid'";
+    $resultunpaid = mysqli_query($con,$sql3);
+    $r=mysqli_fetch_assoc($resultunpaid);
+    if($r > 0){
+        $totalunpaid = $r['count_unpaid'];
+    }else{
+        $totalunpaid=0;
+    }
+    return $totalunpaid;
+}
+
+//total specific payment get per doctor
+function gettotalspecificpayment($doctorname){
+    $con = getconection();
+    $sql4 = "select SUM(Payment) as total_specific_payment from invoice where Doctor_Name = '$doctorname' and Status = 'Paid'";
+    $result= mysqli_query($con,$sql4);
+    $r = mysqli_fetch_assoc($result);
+    $total_specific_doctor_payment = $r['total_specific_payment'];
+    return $total_specific_doctor_payment;
+}
+
+//update invoice list
+function getupdateinvoice($appointmentid,$patientname,$doctorname,$serial,$payment,$appointmentdate,$paymenttype,$status,$slno){
+    $con = getconection();
+    $sql ="UPDATE invoice SET `Appointment_ID`='$appointmentid',`Patient_Name`='$patientname',`Doctor_Name`='$doctorname',`Serial`='$serial',`Payment`='$payment',`Appointment_Date`='$appointmentdate',`Payment_Type`='$paymenttype',`Status`='$status' WHERE SLNo=$slno";
+    mysqli_query($con,$sql);
+}
+
+//select invoice for update
+function getselectinvoice($editinvoice){
+    $con = getconection();
+    $sql = "select * from invoice where SLNo = $editinvoice;";
+    $result=mysqli_query($con,$sql);
+    return $result;
+}
+
+//view doctor appointment list
+function getdoctorappointment(){
+    $con = getconection();
+    $sql = "SELECT Doctor_name, Department, Available_Day FROM doctor";
+    $result = mysqli_query($con,$sql);
+    return $result;
+}
+
+//search doctor 
+function getsearchdoctor($name){
+    $con = getconection();
+    $sql = "SELECT Doctor_name, Department, Available_Day FROM doctor where Doctor_name='$name'";
+    $result = mysqli_query($con,$sql);
+    return $result;
+}
 ?>
