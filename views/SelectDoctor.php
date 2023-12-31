392621e41d2ea912d452463cdf2856f39ec0eb72
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-12-31 12:17:22 +0600

    Update SelectDoctor.php

--- views/SelectDoctor.php
+++ views/SelectDoctor.php
@@ -5,32 +5,34 @@ include '../controllers/SelectDoctorControl.php';
 <html>
 <head>
     <title>Schedule Date</title>
+    <link rel="stylesheet" href="../css/Dashboardstyle.css">
 </head>
 <body>
     <form method="get" action="../controllers/SelectDoctorControl.php">
+    <h1>Date </h1>
+                        <ul>
+                        <li><a href="Dashboard.php" class="navbar">Dashboard</a></li>
+                        <li><a href="PatientList.php" class="navbar">Patient</a></li>
+                        <li><a href="AppointmentView.php" class="navbar">Appointment</a></li>
+                        <li><a href="InvoiceDashboard.php" class="navbar">Transaction</a><br> <br></li>
+                        <a href="../controllers/Logout.phps" id="logout">Logout</a>
+                        </ul>
         <center>
-            <table border="1" style="width: 700px;">
-                <tr>
-                    <td style="height: 50px;text-align:right;">
-                        <a href="../controllers/Logout.phps">Logout</a>
+            <table border="1" id="outertable">
+                <tr id="outertr1">
+                    <td>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <h3>Date </h3><br>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
-                        <a href="Dashboard.php">Dashboard</a>&nbsp &nbsp &nbsp 
-                        <a href="PatientList.php">Patient</a>&nbsp &nbsp &nbsp
-                        <a href="AppointmentView.php">Appointment</a> &nbsp &nbsp &nbsp
-                        <a href="InvoiceDashboard.php">Transaction</a>&nbsp &nbsp &nbsp<br> <br><br><br>
-                        <!-- <a href="Notification.php">Notification</a>&nbsp &nbsp &nbsp
-                        <a href="Settings.php">Settings</a>&nbsp &nbsp &nbsp <br><br><br><br> -->
                         <center>
-                            <table border="1" style="width: 200px;">
+                            <table border="1" id="innertable1">
                                 <tr>
                                     <td><br>
-                                    <input type="search" name="doctorname" id="search" placeholder="Doctor name">
+                                    <input type="search" name="doctorname" id="input" placeholder="Doctor name">
                                     <br><br>
-                                    <input type="submit" name="click" value="click">
+                                    <input type="submit" id="button" name="click" value="click">
+                                    <!-- <button name="click">click</button> -->
                                     <br><br>
                                     </td>
                                 </tr>
