b49e00f851a3e37393014df00766905ce922e57b
Mohammad Bin Harun <79358840+Mohammad-coders@users.noreply.github.com>
2023-12-31 12:16:36 +0600

    Update Login.php

--- views/Login.php
+++ views/Login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+$_SESSION['login_time'] = time();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,22 +9,26 @@
     <link rel="stylesheet" href="../css/Loginstyle.css">
 </head>
 <body>
+    <h2>Clinic Management System</h2>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIetdbu7nsPZ5rYejZLsKdkMNPcfRBhM8hJA&usqp=CAU" alt="">
     <form method="post" action="../Controllers/LoginControl.php">
-        <center>
         <table border="1" id="outertable">
-            <tr>
-                <td id="heading">Login</td>
-            </tr>
-            <tr>
+            <tr id="outerrow">
+            <h2>Login</h2>
                 <td>
-                    <center>
-                        Username: <input type="text" name="username"><br><br>
-                        Password: <input type="password" name="security"><br><br>
-                        <input type="submit" name="loginbtn" value="login"><br><br>
-                    </center>                 
+                <center>
+                    <div>
+                        <label for="Username">Username:</label>
+                        <input type="text" name="username" id="input"><br>
+                        <label for="Password">Password:</label>
+                        <input type="password" name="security" id="input"><br>
+                        <input type="submit" name="loginbtn" id="loginbtn" value="Login">
+                    </div>
+                </center>   
+                                  
                 </td>
             </tr>
-        </table></center>
+        </table>
     </form>
 </body>
 </html>
